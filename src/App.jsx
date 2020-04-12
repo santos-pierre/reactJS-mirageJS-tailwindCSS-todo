30432d8f4629a05155ec4861e2d397a6fbdb68e4
Pierre Santos <santospierre1305@gmail.com>
2020-04-12 11:39:43 +0200

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <React.Fragment className="relative bg-gray-50 overflow-hidden">
+        <div className="mt-10 mx-auto max-w-screen-xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 xl:mt-28">
+          <div className="text-center">
+            <h2 className="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl">
+              Geting Started Developing In
+              <br className="xl:hidden" />
+              <span className="text-indigo-600">React/MirageJs/TailwindCSS</span>
+            </h2>
+          </div>
+        </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
