4650983bc00717199995971d2d2b8b39e7c226aa
Pierre Santos <santospierre1305@gmail.com>
2020-04-12 17:04:23 +0200

    Header Todo - First Step (WIP)

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,18 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <React.Fragment className="relative bg-gray-50 overflow-hidden">
-        <div className="mt-10 mx-auto max-w-screen-xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 xl:mt-28">
-          <div className="text-center">
-            <h2 className="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl">
-              Geting Started Developing In
-              <br className="xl:hidden" />
-              <span className="text-indigo-600">React/MirageJs/TailwindCSS</span>
-            </h2>
-          </div>
+    <div className="relative bg-gray-50">
+      <div className="mt-10 mx-auto max-w-screen-xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 xl:mt-28">
+        <div className="w-11/12 shadow mx-auto p-10">
+          <h2 className="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl mb-2">
+            <span className="text-indigo-600">Your </span> Todos
+          </h2>
+          <hr className="mb-5"/>
         </div>
-    </React.Fragment>
+      </div>
+    </div>
   );
 }
 
